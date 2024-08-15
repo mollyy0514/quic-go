71e7bbe1e23041cb0bb69152fd8659732dd8070f
mollyy0514 <91655983+mollyy0514@users.noreply.github.com>
2024-08-15 10:30:59 +0800

    commented adding feedback frame part

--- mySocket/client/client.go
+++ mySocket/client/client.go
@@ -22,7 +22,7 @@ const SERVER = "127.0.0.1"
 // const SERVER = "140.112.20.183" // 249 public IP
 const PORT_UL = 4200
 const PORT_DL = 4201
-const SLEEPTIME = 10
+const SLEEPTIME = 2
 
 var serverAddr_ul string = fmt.Sprintf("%s:%d", SERVER, PORT_UL)
 var serverAddr_dl string = fmt.Sprintf("%s:%d", SERVER, PORT_DL)
@@ -234,7 +234,7 @@ func SendPacket(stream quic.Stream, message []byte) {
 
 func Client_send(stream quic.Stream) {
 	// Duration to run the sending process
-	duration := 20 * time.Second
+	duration := 200 * time.Second
 	seq := 1
 	start_time := time.Now()
 	euler := 271828
